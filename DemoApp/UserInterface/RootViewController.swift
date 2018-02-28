3adfdce1274576297d961a158a19334d8cf9986a
Tony Mu <tonymu@Tonys-iMac.local>
2018-02-28 22:14:45 +1300

    Added UIViewExtension

--- DemoApp/UserInterface/RootViewController.swift
+++ DemoApp/UserInterface/RootViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class RootViewController: UIViewController {
 
+  lazy var collectionView: UICollectionView = {
+    let layout = UICollectionViewFlowLayout()
+    let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+    cv.backgroundColor = UIColor.red
+    return cv
+  }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    view.addSubview(collectionView)
+    
+    collectionView.anchorToTop(view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor)
   }
 
   override func didReceiveMemoryWarning() {
